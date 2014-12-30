1ff1cb7c09ed1cefa4afb9bca7262e622b4b0732
Cristian Felix <cristian.felix@gmail.com>
2014-12-29 22:03:28 -0200

    Node App

--- Processing/Scripts-Js/GenerateIndexData1.js
+++
@@ -1,8 +0,0 @@
-db.MiniBusinessTop50.aggregate([
-    { $unwind: "$reviews"},
-    { $project: {_id : "$reviews._id", tuples: "$reviews.tuples", votes: "$reviews.votes", stars: "$reviews.stars"}},
-    { $out: "reviews_Top50Search"}
-])
-db.reviews_Top50Search.ensureIndex({ "tuples.feature": 1, "tuples.qualifier": 1, "tuples.modifier": 1})
-
-
--- Processing/Scripts-Js/ReviewsWithQualifiersByBusiness.js
+++
@@ -1,9 +0,0 @@
-db.reviews_processed.aggregate([
-    {$match: { $or: [ {CategoryFirst: "Food"}, {CategoryFirst: "Restaurants"}] }},
-    {$unwind: "$features" },
-    {$project: { _id: 0, review_id: "$review_id", business_id: "$business_id", feature: "$features.feature.lemma", qualifiers: "$features.qualifiers", QualyCount: { $size:  "$features.qualifiers"} }},
-    {$match: {QualyCount: {$gte:1}}},
-    {$group: { _id: { review_id: "$review_id", business_id: "$business_id"}}},
-    {$group: { _id: "$_id.business_id", count: {$sum: 1}}},
-    {$out: "RestaurantsHasQualifiersCount"}
-], {allowDiskUse:true});
