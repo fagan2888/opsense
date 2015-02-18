0712832ef4975543ed248aa6c94f57d68183fcf5
Cristian Felix <cristian.felix@gmail.com>
2015-02-17 21:39:08 -0500

    Fix quote

--- app/index.js
+++ app/index.js
@@ -142,11 +142,11 @@ function searchData(db, sort, feature, qualifier, modifier, callback, collName) 
         var feature = quote(request.param('f'));
         var qualifier = quote(request.param('q'));
         var modifier = quote(request.param('m'));
-        var sort = quote(request.param('s'));
+        var sort = request.param('s');
         var col = request.param('col') || 'reviews_Top50SearchHealth';
         
         
-        console.log("started " + new Date());
+        console.log(sort);
         searchData(db, sort, feature, qualifier, modifier,function(err, results){
             if(err) throw err;
             console.log("will send " + new Date());
